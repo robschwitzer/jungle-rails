f158cd2698336502f9f7d4be5bbd755395a6f91c
Rob Schwitzer <deadinthewater@me.com>
2018-06-23 03:39:38 +0000

    hunting bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  # validates :name, presence: true
-  # validates :email, presence: true
-  # validates :password, presence: true
-  # validates :password_confirmation, presence: true
+  has_many  :reviews
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 
 end
--- db/migrate/20180622234228_create_reviews.rb
+++ db/migrate/20180622234228_create_reviews.rb
@@ -3,7 +3,7 @@ class CreateReviews < ActiveRecord::Migration
     create_table :reviews do |t|
       t.integer :product_id
       t.integer :user_id
-      t.string :description
+      t.string  :description
       t.integer :rating
 
       t.timestamps null: false
