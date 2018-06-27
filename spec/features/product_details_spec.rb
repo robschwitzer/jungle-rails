d206bd9a8baa45734095ca7b4cd675b01c988792
Rob Schwitzer <deadinthewater@me.com>
2018-06-27 01:33:56 +0000

    product details page rspec

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+
+  end
+
+    scenario "They see product details page" do
+      # ACT
+      visit root_path
+
+        first("article.product").find_link('Details').click
+        expect(page).to have_css('section.products-show')
+      # DEBUG / VERIFY
+      save_screenshot
+    end
+
+end
